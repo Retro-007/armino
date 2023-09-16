9398d336793cc1b5fcbd583368e900a6ceb4621d
raihan <raihan@aventusinformatics.com>
2023-09-16 22:33:04 +0530

    as

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Header from "./components/Header"
 import Content from "./components/Content"
-// import gsap from 'gsap';
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import ScrollSmoother from "gsap-trial/ScrollSmoother";
-// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollSmoother from "gsap-trial/ScrollSmoother";
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 function App() {
 
 
 
-  // useEffect(() => {
-  //   ScrollTrigger.normalizeScroll(true)
+  useEffect(() => {
+    ScrollTrigger.normalizeScroll(true)
 
-  //   // Initialize ScrollTrigger
-  //   // eslint-disable-next-line no-unused-vars
-  //   let smoother = ScrollSmoother.create({
-  //     smooth: 1,
-  //     effects: true,
-  //     normalizeScroll: true
-  //   });
+    // Initialize ScrollTrigger
+    // eslint-disable-next-line no-unused-vars
+    let smoother = ScrollSmoother.create({
+      smooth: 1,
+      effects: true,
+      normalizeScroll: true
+    });
 
-  //   // pin box-c when it reaches the center of the viewport, for 300px
-  //   ScrollTrigger.create({
-  //     pin: true,
-  //     start: "center center",
-  //     end: "+=300",
-  //   });
+    // pin box-c when it reaches the center of the viewport, for 300px
+    ScrollTrigger.create({
+      pin: true,
+      start: "center center",
+      end: "+=300",
+    });
 
-  // }, []);
+  }, []);
 
   return (
     <React.Fragment>
